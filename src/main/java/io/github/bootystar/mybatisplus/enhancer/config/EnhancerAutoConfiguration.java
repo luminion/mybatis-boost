35cbaddfdc064f783f690f4f16597cf67ebe1925
lo <604647004@qq.com>
2025-04-20 09:08:14 +0800

    调整mapper工具

--- src/main/java/io/github/bootystar/mybatisplus/enhancer/config/EnhancerAutoConfiguration.java
+++ src/main/java/io/github/bootystar/mybatisplus/enhancer/config/EnhancerAutoConfiguration.java
@@ -1,19 +1,13 @@
 package io.github.bootystar.mybatisplus.enhancer.config;
 
+import io.github.bootystar.mybatisplus.enhancer.util.MapperHelper;
 import lombok.extern.slf4j.Slf4j;
-import org.aopalliance.aop.Advice;
-import org.apache.ibatis.builder.xml.XMLMapperBuilder;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * @author bootystar
@@ -23,22 +17,15 @@ import java.io.InputStream;
 @ConditionalOnClass({SqlSessionFactory.class})
 public class EnhancerAutoConfiguration implements ApplicationContextAware {
 
-
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         SqlSessionFactory sqlSessionFactory = applicationContext.getBean(SqlSessionFactory.class);
-        Configuration configuration = sqlSessionFactory.getConfiguration();
-        // 指定Mapper XML文件的路径
-        String resource = "mapper/DynamicMapper.xml";
-        try (InputStream inputStream = Resources.getResourceAsStream(resource)){
-            // 创建XMLMapperBuilder并解析Mapper XML文件
-            XMLMapperBuilder mapperBuilder = new XMLMapperBuilder(inputStream, configuration, resource, configuration.getSqlFragments());
-            mapperBuilder.parse();
+        boolean b = MapperHelper.initSqlFragment(sqlSessionFactory);
+        if (b){
             log.debug("DynamicMapper sqlFragments Configured");
-        } catch (IOException e) {
-            log.error("error creating DynamicMapper sqlFragments",e);
+        }else{
+            log.error("DynamicMapper sqlFragments Configured Failed, dynamic sql may not work");
         }
     }
-
 
 }
--- src/main/java/io/github/bootystar/mybatisplus/enhancer/util/MapperHelper.java
+++ src/main/java/io/github/bootystar/mybatisplus/enhancer/util/MapperHelper.java
@@ -2,27 +2,38 @@ package io.github.bootystar.mybatisplus.enhancer.util;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.core.toolkit.LambdaUtils;
-import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
 import com.baomidou.mybatisplus.extension.service.IService;
 import io.github.bootystar.mybatisplus.enhancer.core.base.EnhancedQuery;
-import org.apache.ibatis.reflection.property.PropertyNamer;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.builder.xml.XMLMapperBuilder;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSessionFactory;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author bootystar
  */
+@Slf4j
 public abstract class MapperHelper {
 
-    public static <T> String getXmlContent(Class<? extends EnhancedQuery<?>> enhancedQueryClass) {
-        return getXmlContent(enhancedQueryClass, new HashMap<>());
+    public static boolean initSqlFragment(SqlSessionFactory sqlSessionFactory) {
+        Configuration configuration = sqlSessionFactory.getConfiguration();
+        String resource = "mapper/DynamicMapper.xml";
+        try (InputStream inputStream = Resources.getResourceAsStream(resource)) {
+            XMLMapperBuilder mapperBuilder = new XMLMapperBuilder(inputStream, configuration, resource, configuration.getSqlFragments());
+            mapperBuilder.parse();
+            return true;
+        } catch (IOException e) {
+            log.error("error creating DynamicMapper sqlFragments", e);
+            return false;
+        }
     }
 
-    @SuppressWarnings("unchecked")
-    public static <T> String getXmlContent(Class<? extends EnhancedQuery<?>> enhancedQueryClass, Map<SFunction<T, ?>, Boolean> sortMap) {
+    @SuppressWarnings("unchecked" )
+    public static <T> String getSelectContent(Class<? extends EnhancedQuery<?>> enhancedQueryClass) {
         Class<?> voClass = MybatisPlusReflectHelper.resolveTypeArguments(enhancedQueryClass, EnhancedQuery.class)[0];
         Class<?>[] mapperClasses = MybatisPlusReflectHelper.resolveTypeArguments(enhancedQueryClass, BaseMapper.class);
         Class<?>[] serviceClasses = MybatisPlusReflectHelper.resolveTypeArguments(enhancedQueryClass, IService.class);
@@ -34,26 +45,17 @@ public abstract class MapperHelper {
         } else {
             throw new IllegalArgumentException("no base entity info in " + enhancedQueryClass.getName());
         }
-        return getXmlContent(entityClass, voClass, sortMap);
+        return getSelectContent(entityClass, voClass);
     }
 
-    public static String getXmlContent(Class<?> entityClass, Class<?> voClass) {
-        return getXmlContent(entityClass, voClass, null);
+    public static <T> String getSelectContent(Class<T> entityClass, Class<?> voClass) {
+        return "    <select id=\"voSelectByXml\" resultType=\"" + voClass.getName() + "\">\n" +
+                getSqlContent(entityClass) +
+                "    </select>"
+                ;
     }
 
-    public static <T> String getXmlContent(Class<T> entityClass, Class<?> voClass, Map<SFunction<T, ?>, Boolean> sortMap) {
-        AtomicReference<String> orderBySqlRef = new AtomicReference<>("");
-        if (sortMap != null && !sortMap.isEmpty()) {
-            sortMap.entrySet().stream().map(e -> {
-                SFunction<T, ?> key = e.getKey();
-                String columnName = PropertyNamer.methodToProperty(LambdaUtils.extract(key).getImplMethodName());
-                return String.format("a.%s%s", columnName, e.getValue() ? " DESC" : "");
-            }).reduce((e1, e2) -> e1 + ", " + e2).ifPresent(orderBySqlRef::set);
-        }
-        String orderBySql = orderBySqlRef.get();
-        if (!orderBySql.isEmpty()) {
-            orderBySql = "            , " + orderBySql + "\n";
-        }
+    public static <T> String getSqlContent(Class<T> entityClass) {
         String tableName;
         TableName annotation = entityClass.getAnnotation(TableName.class);
         if (annotation != null && !annotation.value().isEmpty()) {
@@ -61,69 +63,73 @@ public abstract class MapperHelper {
         } else {
             tableName = entityClass.getName();
         }
-        return
-//                "    <sql id=\"selectFragment\">\n" +
-//                "        <if test=\"param1 != null\">\n" +
-//                "            <foreach collection=\"param1\" item=\"gen\">\n" +
-//                "                <if test=\"gen.conditions != null and gen.conditions.size() > 0\">\n" +
-//                "                    <trim prefix=\"AND ( \" suffix=\" )\" prefixOverrides=\"AND|OR\">\n" +
-//                "                        <foreach collection=\"gen.conditions\" item=\"item\">\n" +
-//                "                            <choose>\n" +
-//                "                                <when test=\"item.isOr()\">\n" +
-//                "                                    OR\n" +
-//                "                                </when>\n" +
-//                "                                <otherwise>\n" +
-//                "                                    AND\n" +
-//                "                                </otherwise>\n" +
-//                "                            </choose>\n" +
-//                "                            <choose>\n" +
-//                "                                <when test=\"item.operator=='IN' or item.operator=='NOT IN'\">\n" +
-//                "                                    ${item.field} ${item.operator}\n" +
-//                "                                    <foreach collection=\"item.value\" item=\"val\" separator=\",\" open=\"(\" close=\")\">\n" +
-//                "                                        #{val}\n" +
-//                "                                    </foreach>\n" +
-//                "                                </when>\n" +
-//                "                                <when test=\"item.operator=='IS NULL' or item.operator=='IS NOT NULL'\">\n" +
-//                "                                    ${item.field} ${item.operator}\n" +
-//                "                                </when>\n" +
-//                "                                <otherwise>\n" +
-//                "                                    ${item.field} ${item.operator} #{item.value}\n" +
-//                "                                </otherwise>\n" +
-//                "                            </choose>\n" +
-//                "                        </foreach>\n" +
-//                "                    </trim>\n" +
-//                "                </if>\n" +
-//                "            </foreach>\n" +
-//                "        </if>\n" +
-//                "    </sql>\n" +
-//                "\n" +
-//                "    <sql id=\"sortFragment\">\n" +
-//                "        <if test=\"param1 != null and param1.sorts != null and param1.sorts.size() > 0\">\n" +
-//                "            <foreach collection=\"param1.sorts\" item=\"item\" separator=\",\" close=\",\">\n" +
-//                "                ${item.field}\n" +
-//                "                <if test=\"item.isDesc()\">\n" +
-//                "                    DESC\n" +
-//                "                </if>\n" +
-//                "            </foreach>\n" +
-//                "        </if>\n" +
-//                "    </sql>\n" +
-//                "\n" +
-                "    <select id=\"voSelectByXml\" resultType=\"" + voClass.getName() + "\">\n" +
-                "        SELECT\n" +
+        return getSqlContent(tableName);
+    }
+
+    public static String getSqlContent(String tableName) {
+        return "        SELECT\n" +
                 "        a.*\n" +
                 "        FROM\n" +
                 "        " + tableName + " a\n" +
                 "        <where>\n" +
-                "            <include refid=\"io.github.bootystar.mybatisplus.enhancer.core.DynamicMapper.selectFragment\"/>\n" +
-//                "            AND a.deleted = 0\n" + // 逻辑删除
+                "            <include refid=\"io.github.bootystar.mybatisplus.enhancer.core.DynamicMapper.dynamicSelect\"/>\n" +
                 "        </where>\n" +
                 "        <trim prefix=\"ORDER BY\" prefixOverrides=\",\">\n" +
-                "            <include refid=\"io.github.bootystar.mybatisplus.enhancer.core.DynamicMapper.sortFragment\"/>\n" +
-                orderBySql +  // 默认排序
-                "        </trim>\n" +
-                "    </select>"
+                "            <include refid=\"io.github.bootystar.mybatisplus.enhancer.core.DynamicMapper.dynamicSort\"/>\n" +
+                "        </trim>\n"
                 ;
     }
 
+    public static String getDynamicSelectFragment() {
+        return "    <sql id=\"dynamicSelect\">\n" +
+                "        <if test=\"param1 != null\">\n" +
+                "            <foreach collection=\"param1\" item=\"gen\">\n" +
+                "                <if test=\"gen.conditions != null and gen.conditions.size() > 0\">\n" +
+                "                    <trim prefix=\"AND (\" suffix=\")\" prefixOverrides=\"AND|OR\">\n" +
+                "                        <foreach collection=\"gen.conditions\" item=\"item\">\n" +
+                "                            <choose>\n" +
+                "                                <when test=\"item.isOr()\">\n" +
+                "                                    OR\n" +
+                "                                </when>\n" +
+                "                                <otherwise>\n" +
+                "                                    AND\n" +
+                "                                </otherwise>\n" +
+                "                            </choose>\n" +
+                "                            <choose>\n" +
+                "                                <when test=\"item.operator == 'IN' or item.operator == 'NOT IN'\">\n" +
+                "                                    ${item.field} ${item.operator}\n" +
+                "                                    <foreach collection=\"item.value\" item=\"val\" separator=\",\" open=\"(\" close=\")\">\n" +
+                "                                        #{val}\n" +
+                "                                    </foreach>\n" +
+                "                                </when>\n" +
+                "                                <when test=\"item.operator == 'IS NULL' or item.operator == 'IS NOT NULL'\">\n" +
+                "                                    ${item.field} ${item.operator}\n" +
+                "                                </when>\n" +
+                "                                <otherwise>\n" +
+                "                                    ${item.field} ${item.operator} #{item.value}\n" +
+                "                                </otherwise>\n" +
+                "                            </choose>\n" +
+                "                        </foreach>\n" +
+                "                    </trim>\n" +
+                "                </if>\n" +
+                "            </foreach>\n" +
+                "        </if>\n" +
+                "    </sql>"
+                ;
+    }
+
+    public static String getDynamicSortFragment() {
+        return "    <sql id=\"dynamicSort\">\n" +
+                "        <if test=\"param1 != null and param1.sorts != null and param1.sorts.size() > 0\">\n" +
+                "            <foreach collection=\"param1.sorts\" item=\"item\" separator=\",\" open=\",\">\n" +
+                "                ${item.field}\n" +
+                "                <if test=\"item.isDesc()\">\n" +
+                "                    DESC\n" +
+                "                </if>\n" +
+                "            </foreach>\n" +
+                "        </if>\n" +
+                "    </sql>"
+                ;
+    }
 
 }
