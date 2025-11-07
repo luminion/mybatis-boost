d0e8f47fc8c3b4a23696d68a1d9a7834190fae2f
lo <604647004@qq.com>
2025-11-07 10:03:31 +0800

    feat(mybatisplus): 添加 BoosterService 接口的 selectByBooster 方法实现
    - 引入 Wrapper 和 List 类型依赖
    - 实现 selectByBooster 默认方法，调用底层 BoosterBaseMapper- 添加方法注释继承标记- 强制类型转换获取 BoosterBaseMapper 实例

--- src/main/java/io/github/luminion/sqlbooster/extension/mybatisplus/BoosterService.java
+++ src/main/java/io/github/luminion/sqlbooster/extension/mybatisplus/BoosterService.java
@@ -1,6 +1,9 @@
 package io.github.luminion.sqlbooster.extension.mybatisplus;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import io.github.luminion.sqlbooster.model.api.Wrapper;
+
+import java.util.List;
 
 /**
  * 针对MyBatis-Plus 的 IService 扩展接口.
@@ -14,4 +17,14 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 1.0.0
  */
 public interface BoosterService<T, V> extends IService<T>, MybatisPlusBooster<T, V> {
+
+    /**
+     * {@inheritDoc}
+     * @since 1.0.0
+     */
+    @Override
+    default List<V> selectByBooster(Wrapper<T> wrapper, Object page) {
+        BoosterBaseMapper<T, V> baseMapper = (BoosterBaseMapper<T, V>) getBaseMapper();
+        return baseMapper.selectByBooster(wrapper, page);
+    }
 }
